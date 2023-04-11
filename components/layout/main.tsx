e0cab2bdcbd9cad3a855db96b30bd56502a45e02
vuddt <vuddt@tahospital.vn>
2023-04-11 15:40:46 +0700

    Change name

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -1,6 +1,6 @@
 import { LayoutProps } from '~/models';
 import { Layout } from 'antd';
-import { Header } from '../common/Header';
+import { Header } from '../common/Header1';
 const { Content } = Layout;
 
 export function MainLayout({ children }: LayoutProps) {
