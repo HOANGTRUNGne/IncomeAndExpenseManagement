e2a6c715c89e9d9e8365a789495fd7c4f4c09893
vuddt <vuddt@tahospital.vn>
2023-04-11 16:15:38 +0700

    🐛 [FIX]  : Load css

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-const withLess = require('next-with-less');
 
 const nextConfig = {
     reactStrictMode: true,
-    lessLoaderOptions: {},
     images: {
         domains: ['ant-cra.cremawork.com', 'cremawork.com'],
     },
 };
 
-module.exports = withLess(nextConfig);
+module.exports = nextConfig;
