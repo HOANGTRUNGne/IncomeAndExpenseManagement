c9362d4a578a41fc33a93747af7085db6eabb2ec
vuddt <vuddt@tahospital.vn>
2023-04-11 15:41:52 +0700

    Fix name

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -1,6 +1,6 @@
 import { LayoutProps } from '~/models';
 import { Layout } from 'antd';
-import { Header } from '../common/Header1';
+import { Header } from '../common/Header';
 const { Content } = Layout;
 
 export function MainLayout({ children }: LayoutProps) {
