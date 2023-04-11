080d9e024f3ecce7de38f4a4e230367192001793
vuddt <vuddt@tahospital.vn>
2023-04-11 15:34:07 +0700

    🐛 [FIX] : Path module

--- components/common/header/components/MyProfile/index.tsx
+++ components/common/header/components/MyProfile/index.tsx
@@ -1,5 +1,7 @@
 import { Avatar, Popover } from 'antd';
+import Link from 'next/link';
 import { useState } from 'react';
+import { APP_ROUTES } from '~/constant';
 
 export interface MyProfileProps {}
 
@@ -13,13 +15,15 @@ export default function MyProfile(props: MyProfileProps) {
     const contents = () => (
         <ul className="m-0 list-none p-0">
             <li>My Profile</li>
-            <li>Logout</li>
+            <li>
+                <Link href={APP_ROUTES.SIGNIN}>Logout</Link>
+            </li>
         </ul>
     );
 
     return (
         <Popover content={contents} trigger="click" open={open} onOpenChange={handleOpenChange}>
-            <Avatar className="ml-4"></Avatar>
+            <Avatar className="ml-4 cursor-pointer"></Avatar>
         </Popover>
     );
 }
---
+++ pages/signin.tsx
@@ -0,0 +1,9 @@
+import { MainLayout } from '~/components/layout';
+
+export interface SigninProps {}
+
+export default function Signin(props: SigninProps) {
+    return <div>Signin</div>;
+}
+
+Signin.Layout = MainLayout;
